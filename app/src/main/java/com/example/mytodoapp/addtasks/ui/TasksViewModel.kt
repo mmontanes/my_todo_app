9daad932fad1174cd569b43f3e7f6158303cb1d1
Manuel Montañés <montanes.manuel@gmail.com>
2024-04-02 18:29:27 +0200

    Add states for State Flow

--- app/src/main/java/com/example/mytodoapp/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/mytodoapp/addtasks/ui/TasksViewModel.kt
@@ -4,16 +4,31 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mytodoapp.addtasks.domain.AddTaskUseCase
+import com.example.mytodoapp.addtasks.domain.GetTaskUseCase
+import com.example.mytodoapp.addtasks.ui.TasksUIState.*
 import com.example.mytodoapp.addtasks.ui.model.TaskModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class TasksViewModel @Inject constructor() : ViewModel() {
+class TasksViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    getTaskUseCase: GetTaskUseCase
+) : ViewModel() {
+
+    val uiState: StateFlow<TasksUIState> = getTaskUseCase().map (::Success)
+        .catch { Error(it) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
+
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
 
-    // Esta solución va mejor que los LiveData normales.
-    // Una alternativa a esta solución es usar Flow.
-    // TODO: Implementar Flow
     private val _tasksList = mutableStateListOf<TaskModel>()
     val tasksList: List<TaskModel> = _tasksList
 
@@ -29,6 +44,10 @@ class TasksViewModel @Inject constructor() : ViewModel() {
         // Add task to the database
         _tasksList.add(TaskModel(content = task))
         _showDialog.value = false
+
+        viewModelScope.launch {
+            addTaskUseCase(TaskModel(content = task))
+        }
     }
 
     fun onTaskCheckBoxClicked(taskModel: TaskModel) {
